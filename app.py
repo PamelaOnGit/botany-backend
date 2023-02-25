8260b423bcded25c0d17f2c15756622f73543cdd
Pamela Smith <pammyokeeffe@gmail.com>
2023-02-25 12:21:55 +0000

    building production environment

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from flask_cors import CORS
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
@@ -6,6 +7,7 @@ from flask_bcrypt import Bcrypt
 from config.environment import db_URI
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = db_URI
 app.config['SQLALCHEMY_TRACK_MY_MODIFICATIONS'] = False
--- config/environment.py
+++ config/environment.py
@@ -1,6 +1,18 @@
-db_URI = 'postgresql://localhost:5432/botany_project'
+from os import environ
+# import load_env, this function loads an env file from a path.
+from dotenv import load_dotenv
 
-admin_username = 'Pam'
-admin_password = 'pugshoelightbox'
+# Here we get a filepath from either the ENV_FILE passed through, or .env as a default. 
+ENVIRONMENT_FILE = environ.get('ENV_FILE') or '.env'
 
-secret = 'tablecrashplantsoileverywheredog'
+# Here we are passing through the env file to load from. override=True means the env file will override .env, if .env exists. 
+load_dotenv(ENVIRONMENT_FILE, override=True)
+
+# Now, environ.get will retrieve variables from the file we provided.
+db_URI = environ.get('DB_URI')
+secret = environ.get('SECRET')
+
+
+
+
+
